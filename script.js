cc0589377c4e2cc783bf6b4cc8e1913c46cb71b6
Arifin1923250058 <72389552+Arifin1923250058@users.noreply.github.com>
2020-11-09 09:29:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.write("<center><h1>Terima Kasih!!! </h1></center>");
+document.getElementById("ttp").innerHTML = "<center>Pemrograman Web I Arifin 1923250058</center>";
+document.getElementById("ttp").style.textShadow = "0 0 3px #ff7f50, 0 0 5px #9932cc";
